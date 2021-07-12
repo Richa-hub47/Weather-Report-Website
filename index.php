bf06d18492a795cc4622bacb1d7ae81058c92b46
Richa-hub47 <78342322+Richa-hub47@users.noreply.github.com>
2021-07-12 13:01:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head><title>Weather Report</title>
+<link  href="style.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+</head>
+
+<body>
+
+
+
+	
+       <div class="banner-image">
+           <div class="inner-banner-image">
+              <center>
+               <div class="banner_content">
+                    WEATHER REPORT
+                </div>
+                  
+	<center><br><br>
+		<form method="GET" action="get.php">
+		<h1>Type Place Name</h1>
+		<br><p>For Example, Kolkata</p>
+			<input type="text" name="q" required>
+                        </br></br>
+			<input type="submit" name="submit">
+		</form>
+	</center>
+                  
+              </center>
+           </div>
+        </div>
+    
+	
+</body>
+</html>
---
+++ weathertest.php
@@ -0,0 +1,19 @@
+<?php
+$apiKey = "f9114dfd33c1a7d637ea07a6b34d606e";
+$cityId = "524901";
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+?>
